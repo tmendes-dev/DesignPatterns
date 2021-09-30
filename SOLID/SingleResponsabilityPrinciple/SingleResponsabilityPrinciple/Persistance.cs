5f47dcd83eaed23458731afc1229741a4dc5fcd5
Thomas Mendes <th.mendes1997@gmail.com>
2021-09-30 00:02:55 -0300

    Single Responsability Principle

---
+++ SOLID/SingleResponsabilityPrinciple/SingleResponsabilityPrinciple/Persistance.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+namespace SingleResponsabilityPrinciple
+{
+    public class Persistance
+    {
+        public void SaveToFile(Journal journal, string fileName, bool overwrite = false)
+        {
+            if (overwrite || !File.Exists(fileName))
+                File.WriteAllText(fileName, journal.ToString());
+        }
+    }
+}
---
+++ SOLID/SingleResponsabilityPrinciple/SingleResponsabilityPrinciple/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace SingleResponsabilityPrinciple
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var journal = new Journal();
+
+            journal.AddEntry("Today i studied hard");
+            journal.AddEntry("Tomorrow i'll do the same");
+
+            Console.WriteLine(journal);
+
+            var persistance = new Persistance();
+            var fileName = @"c:\temp\journal.txt";
+
+            persistance.SaveToFile(journal, fileName, true);
+            Process.Start(fileName);
+        }
+    }
+}
