c04b125f8707b591114ecdafac2e022d4e1b409a
Thomas Mendes <th.mendes1997@gmail.com>
2021-10-05 20:31:55 -0300

    Liskov Substution Principle

---
+++ SOLID/LiskovSubstitutionPrinciple/LiskovSubstitutionPrinciple/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LiskovSubstitutionPrinciple
+{
+    partial class Program
+    {
+        static public int Area(Rectangle rectangle) => rectangle.Height * rectangle.Width;  
+        static void Main(string[] args)
+        {
+            var rectangle = new Rectangle(2,3);
+            Console.WriteLine($"{rectangle} has area {Area(rectangle)}");
+
+            Rectangle square = new Square();
+            square.Height = 2;
+            Console.WriteLine($"{square} has area {Area(square)}");
+        }
+    }
+}
---
+++ SOLID/LiskovSubstitutionPrinciple/LiskovSubstitutionPrinciple/Square.cs
@@ -0,0 +1,11 @@
+﻿namespace LiskovSubstitutionPrinciple
+{
+    partial class Program
+    {
+        public class Square : Rectangle
+        {
+            public override int Width { set => base.Width = base.Height = value; }
+            public override int Height { set => base.Height = base.Width = value; }
+        }
+    }
+}
